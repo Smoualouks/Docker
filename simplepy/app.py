59bdc12dcde2d14e7c1184efcd031d132494391f
OMARI Is <omari.ismail@gmail.com>
2023-01-10 12:28:47 +0000

    Dockerfile initiation.

---
+++ simplepy/app.py
@@ -0,0 +1,14 @@
+import time
+import os
+
+# durée d'endormissement
+
+duration = 5
+
+# si la variable d'env est trouvée on écrase duration 
+
+if "SLEEP_DURATION" in os.environ:
+     duration = int(os.environ["SLEEP_DURATION"])
+while True:
+    print("simple app (python) in infinite loop...")
+    time.sleep(duration)
